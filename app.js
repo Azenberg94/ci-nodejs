b3f9572367f552c8553d7dee55076ad932d84be5
Azenberg94 <az.naidja@gmail.com>
2018-05-10 15:09:24 +0200

    Changing value 4

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
- res.send('hello jenkins !');
+ res.send('hello jenkins');
 });
 
 app.listen(process.env.PORT || 5000);
-console.log("Server started on port 5000...");
+console.log("Server started on port 80...");
 
 module.exports = app;
