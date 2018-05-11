4c2c41f1cb2903dc454ac06dd6de11e4aae19fa2
Azenberg94 <az.naidja@gmail.com>
2018-05-11 03:11:39 +0200

    test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello jenkins', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+    request(app).get('/').expect('Hello Azedine !', done);
   });
 });
