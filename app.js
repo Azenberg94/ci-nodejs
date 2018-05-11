7ccb4ac1773104dc7574a9acd594bc7c211f0844
Azenberg94 <az.naidja@gmail.com>
2018-05-11 02:02:03 +0200

    Add dotenv package

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var express = require('express');
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').load();
+}
 
+var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
  res.send('hello jenkins');
 });
 
-app.listen(process.env.PORT || 5000);
-console.log("Server started on port 80...");
+app.listen(process.env.PORT || 80);
+console.log("Server started on port " + process.env.PORT + "...");
 
 module.exports = app;
